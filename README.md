3e9841b43a8f073bd9c7d197b5ebbf481475b0c6
whdudtod1273 <whdudtod1273@naver.com>
2020-10-14 22:36:25 +0900

    snedgrid 연동

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ysgram
 instargram clone with Express + Prisma + React Native
+
+
+
+## User Stories
+
+- [ o ] Create account
+- [ ] Request Secret
+- [ ] Confirm Secret (Login)
+- [ ] Like / Unlike a photo
+- [ ] Comment on a photo
+- [ ] Search by user
+- [ ] Search by location
+- [ ] See user profile
+- [ ] Follow / Unfollow User
+- [ ] See the full photo
+- [ ] Edit my profile
+- [ ] Upload a photo
+- [ ] Edit the photo (Delete)
+- [ ] See the feed
---
+++ src/utils.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, '.env') })
+
+import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer"
+import sgTransport from "nodemailer-sendgrid-transport"
+
+export const generateSecret = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+console.log(process.env.SENDGRID_USERNAME);
+export const sendMail = (email) => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  }
+
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email)
+}
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "whdudtod1273@naver.com",
+    to: adress,
+    subject: "Login Secret for Ysgram",
+    html: `Hello! Your login secret is <b>${secret}</b>. <br/>Copy paste on the app/web to log in`
+  }
+  return sendMail(email)
+}
