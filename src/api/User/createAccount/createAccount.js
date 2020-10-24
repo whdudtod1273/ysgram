821e99addc36180682665f0c6014928f4a3d50bc
whdudtod1273 <whdudtod1273@naver.com>
2020-10-24 15:14:47 +0900

    passport add

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,7 +3,7 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    createAccount: async (_, args) => {
+    createAccount: async (_, args, { request }) => {
       const { name, email, firstName = "", lastName = "", bio = "" } = args;
       const user = await prisma.createUser({
         name,
--- src/server.js
+++ src/server.js
@@ -1,22 +1,21 @@
-// require('dotenv').config();
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, '.env') })
-
+import './env'
 import { GraphQLServer } from 'graphql-yoga';
+import { prisma } from "../generated/prisma-client";
 import logger from "morgan"
 import schema from "./schema"
+import passport from "passport"
+import "./passport"
 import { sendSecretMail } from "./utils";
-
-sendSecretMail("super6915@gmail.com", "123")
+import { authenticateJwt } from './passport';
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
-  schema
+  schema, context: ({ request }) => ({ request })
 })
 
 server.express.use(logger("dev"));
+server.express.unsubscribe(authenticateJwt);
 
 server.start({ port: PORT }, () =>
   console.log(`Server running on http://localhose:${PORT}`)
