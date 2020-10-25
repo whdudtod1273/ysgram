d390bbe5d0ef479c39e354dbbbf650d2c27a863a
whdudtod1273 <whdudtod1273@naver.com>
2020-10-25 22:52:43 +0900

    jwt passport end

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -4,6 +4,7 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     requestSecret: async (_, args, { request }) => {
+      console.log(request);
       const { email } = args;
       const loginSecret = generateSecret();
       try {
