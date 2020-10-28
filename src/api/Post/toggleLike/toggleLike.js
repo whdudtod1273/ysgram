6938ea2aa6bbe5373e80e5c6d6623fe7e93ba5e5
whdudtod1273 <whdudtod1273@naver.com>
2020-10-28 21:40:29 +0900

    post api add

---
+++ src/api/Post/toggleLike/toggleLike.js
@@ -0,0 +1,45 @@
+import { isAuthenticated } from "../../../middlewares"
+
+export default {
+  Mutation: {
+    toggleLike: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { postId } = args;
+      const { user } = request;
+      try {
+        const existingLike = await prisma.$exists.like({
+          AND: [
+            {
+              user: {
+                id: user.id
+              }
+            }, {
+              post: {
+                id: postId
+              }
+            }
+          ]
+        });
+        if (existingLike) {
+          await prisma.deleteManyLikes({})
+        } else {
+          await prisma.createLike({
+            user: {
+              connect: {
+                id: user.id
+              }
+            },
+            post: {
+              connect: {
+                id: postId
+              }
+            }
+          })
+        }
+        return true;
+      } catch {
+        return false
+      }
+    }
+  }
+}
